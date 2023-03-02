6b717c815f03a6d84e4dc6f32305810d9905f91a
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-03-02 09:39:16 +0000

    Add layout

---
+++ src/Components/Navbar.js
@@ -0,0 +1,28 @@
+import { NavLink } from 'react-router-dom';
+
+const links = [
+  { path: '/', text: 'Home' },
+  { path: 'about', text: 'About' },
+  { path: 'profile', text: 'Profile' },
+  { path: 'login', text: 'Login' },
+];
+
+const Navbar = () => (
+  <nav className="navbar">
+    <ul>
+      {links.map((link) => (
+        <li key={link.text}>
+          <NavLink
+            to={link.path}
+            cstyle={({ isActive }) => ({
+              color: isActive ? 'red' : undefined,
+            })}
+          >
+            {link.text}
+          </NavLink>
+        </li>
+      ))}
+    </ul>
+  </nav>
+);
+export default Navbar;

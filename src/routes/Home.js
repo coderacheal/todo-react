cacac56242585925e8c1cf9d08584171e0b81e6f
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-03-02 10:03:49 +0000

    Add missiong file extensions to imports

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,5 @@
-import Header from '../components/Header';
-import TodosLogic from '../components/TodosLogic';
+import Header from '../components/Header.js';
+import TodosLogic from '../components/TodosLogic.js';
 
 const Home = () => (
   <div className="todos">
